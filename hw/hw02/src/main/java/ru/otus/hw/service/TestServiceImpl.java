3c595f3f3b94c6cd6de9189a8b693392ed5efd35
Ruslan <miraxle@yandex.ru>
2024-10-17 19:42:08 +0300

    #hw02 Решение ДЗ№2

--- hw/hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw/hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,11 +1,14 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
 
 @RequiredArgsConstructor
+@Service
 public class TestServiceImpl implements TestService {
 
     private final IOService ioService;
@@ -15,12 +18,16 @@ public class TestServiceImpl implements TestService {
     @Override
     public TestResult executeTestFor(Student student) {
         ioService.printLine("");
-        ioService.printFormattedLine("Please answer the questions below%n");
+        ioService.printFormattedLine("Please answer the questions below:%n");
         var questions = questionDao.findAll();
         var testResult = new TestResult(student);
 
-        for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
+        for (var question : questions) {
+            var studentAnswerString = ioService.readStringWithPrompt(question.text());
+            var studentAnswer = question.answers().stream()
+                    .filter(answer -> StringUtils.equalsIgnoreCase(answer.text(), studentAnswerString))
+                    .findFirst();
+            var isAnswerValid = studentAnswer.isPresent() && studentAnswer.get().isCorrect();
             testResult.applyAnswer(question, isAnswerValid);
         }
         return testResult;
