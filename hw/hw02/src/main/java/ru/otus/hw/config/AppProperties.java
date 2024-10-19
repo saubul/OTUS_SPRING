8650a9c3e7584a9880dcf33cd4be87f9ff1e5140
Ruslan <miraxle@yandex.ru>
2024-10-19 17:34:42 +0300

    #hw02 Исправление ДЗ№2

--- hw/hw02/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw/hw02/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -4,17 +4,15 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
 @Data
-@Configuration
-@PropertySource("classpath:application.properties")
+@Component
 public class AppProperties implements TestConfig, TestFileNameProvider {
 
-    // внедрить свойство из application.properties
     @Value("${test.rightAnswersCountToPass}")
     private int rightAnswersCountToPass;
 
-    // внедрить свойство из application.properties
     @Value("${test.fileName}")
     private String testFileName;
 
